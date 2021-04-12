0d360d1d278296d40a4a4795f0b2bbd9ba8dfc1e
Luiz Santos <luizcsbh@gmail.com>
2021-04-12 12:25:20 -0300

    Subindo scripts

---
+++ scriptclientes.sql
@@ -0,0 +1,5 @@
+INSERT INTO rest_api.clientes(cliente_nome, cliente_telefone, cliente_cep)
+values('Luiz','03198223768','48022-710'),
+('Maria','031997223768','31050-710'),
+('Pedro','031934223768','31023-000'),
+('João','031992349075','31304-200');
